8cac0ec2d1784c515a6e92155cef5b9685b65d82
Shunmugasundaram <129269793+sundarst@users.noreply.github.com>
2025-05-18 14:01:33 +0530

    BankException.java

--- BankException.java
+++ BankException.java
@@ -13,13 +13,13 @@ class InsufficientBalanceException extends Exception
 class Bank
 {
 	String name;
-	int balanceAmount;
+	int depositAmount;
 	int withdrawAmount;
 	
-	Bank(String name,int balanceAmount,int withdrawAmount)
+	Bank(String name,int depositAmount,int withdrawAmount)
 	{
 		this.name=name;
-		this.balanceAmount=balanceAmount;
+		this.depositAmount=depositAmount;
 		this.withdrawAmount=withdrawAmount;
 	}
 	
@@ -27,20 +27,20 @@ class Bank
 
 class Sbi extends Bank
 {
-	Sbi(String name,int balanceAmount,int withdrawAmount)
+	Sbi(String name,int depositAmount,int withdrawAmount)
 	{
-		super(name,balanceAmount,withdrawAmount);
+		super(name,depositAmount,withdrawAmount);
 	}
 		
 	public  void sbiWithdraw() throws InsufficientBalanceException
 	{    
-		if(balanceAmount<withdrawAmount)
+		if(depositAmount<withdrawAmount)
 		{
 			throw new InsufficientBalanceException("Insufficient Balance");
 		}
 		else
 		{
-			int balance= balanceAmount-withdrawAmount;
+			int balance=depositAmount-withdrawAmount;
 			
 			System.out.println("Balace Amount: "+balance );
 		}
@@ -54,11 +54,15 @@ public class BankException
 public static void main(String [] args)
 {
 	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter name");
+	String name = scan.nextLine();
+	System.out.println("Enter deposit Amount");
+	int depsit = scan.nextInt();
 	System.out.println("Enter withdrawAmount");
 	int withdraw= scan.nextInt();
 	try
 	{
-	 Sbi sbi=  new Sbi("sundar",1000,withdraw);
+	 Sbi sbi=  new Sbi(name,depsit,withdraw);
 	 sbi.sbiWithdraw();
 	 
 	}
